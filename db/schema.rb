bd0385f76bb7dab32dd8a37c4e4eef32520643a3
Afiq <mafiqh90@gmail.com>
2016-12-28 23:13:53 +0800

    Scaffolded the tasks

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161227173438) do
+ActiveRecord::Schema.define(version: 20161228150927) do
+
+  create_table "tasks", force: :cascade do |t|
+    t.text     "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
