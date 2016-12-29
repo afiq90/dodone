ed9b5130cef7c21309aa647c23d88b6d05212a38
Afiq <mafiqh90@gmail.com>
2016-12-29 16:30:30 +0800

    Added server side validations on the tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,6 +1,10 @@
 class Task < ApplicationRecord
 	belongs_to :user
 
+	#validate the content and user id 
+	validates :user_id, presence: true
+	validates :content, presence: true
+
 	#specify the use of the auto_html processor.
 	auto_html_for :content do
 		html_escape
